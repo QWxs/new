4cc18aeaf3f674436c1240e4b8e992ba55d916a4
QWxs <2651879595@qq.com>
2019-10-11 20:40:54 +0800

    网口通讯

--- WindowsFormsApplication网口通讯服务器/Form1.cs
+++ WindowsFormsApplication网口通讯服务器/Form1.cs
@@ -36,6 +36,11 @@ namespace WindowsFormsApplication网口通讯服务器
 
 
 
+
+
+
+
+
             IPAddress ip = IPAddress.Parse(textBox1.Text);
             Soc = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             Soc.Bind(new IPEndPoint(ip, 3000));
